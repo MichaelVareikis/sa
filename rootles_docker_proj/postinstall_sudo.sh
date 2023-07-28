e39a3f104c35faf628f946caee2b0baca10204ba
Michael Vareikis <michael.vareikis@axonius.com>
2023-07-28 16:04:38 +0300

    Fix scripts

--- rootles_docker_proj/postinstall_sudo.sh
+++ rootles_docker_proj/postinstall_sudo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-set -ex
-[ ! hash rootlesskit &> /dev/null ] && { echo "rootlesskit not found, make sure you have docker rootless installed"; exit 1; }
-sudo setcap cap_net_bind_service=ep $(which rootlesskit)
+set -e
+[ `id -u` == 0 ] && { echo "Run as non root user"; exit 1; }
+hash rootlesskit &> /dev/null || { echo "rootlesskit not found, make sure you have docker rootless installed"; exit 1; }
+rootlesskit_path=$(which rootlesskit)
+cat << EOF 
+Running this commands with sudo to allow rootlesskit to bind hosts ports below 1024
+sudo setcap cap_net_bind_service=ep $rootlesskit_path
+EOF
+
+sudo setcap cap_net_bind_service=ep $rootlesskit_path
 systemctl --user restart docker
