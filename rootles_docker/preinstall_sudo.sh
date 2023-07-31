6cb8ec6196385daaedee0e3fd2148a2ac4f1538a
Michael Vareikis <michael.vareikis@axonius.com>
2023-07-31 11:55:26 +0300

    Added modules

--- rootles_docker/preinstall_sudo.sh
+++ rootles_docker/preinstall_sudo.sh
@@ -5,14 +5,13 @@ if [ $ID == "ubuntu" ]; then
     echo "Detected OS - $NAME $VERSION"
     sudo apt update
     sudo apt-get install -y dbus-user-session iptables uidmap
-    echo "ip_tables" | sudo tee -a /etc/modules-load.d/modules.conf
-    sudo modprobe ip_tables
 fi
 if [ $ID == "rhel" ]; then
     echo "Detected OS - $NAME $VERSION"
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
     setenforce 0 && getenforce && sestatus
     sudo dnf install -y fuse-overlayfs iptables
-    echo "ip_tables" | sudo tee -a /etc/modules-load.d/modules.conf
-    sudo modprobe ip_tables
 fi
+
+modules=( "ip_tables" "iptable_nat" "nf_conntrack_ipv4" "nf_nat_ipv4" "nf_nat" )
+for mod in ${modules[@]}; do sudo modprobe $mod && echo $mod | sudo tee -a /etc/modules-load.d/modules.conf ||:; done
